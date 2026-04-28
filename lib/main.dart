def6f298b1b9b782f38baa7af0c551cab23a2493
smileeyes1 <smile.eyes1@gmail.com>
2026-04-28 20:23:15 +0300

    Initialize Flutter app with dark theme and text

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    backgroundColor: Colors.black,
+    body: Center(child: Text("نور في وضع الاستعداد", style: TextStyle(color: Colors.white, fontSize: 25))),
+  ),
+  debugShowCheckedModeBanner: false,
+));
